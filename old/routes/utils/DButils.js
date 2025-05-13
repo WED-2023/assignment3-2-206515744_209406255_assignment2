9920f43e9d539812f838f244fb936c187cac58ba
ilayc11 <ilayc11@gmail.com>
2025-05-13 09:20:05 +0300

    added old files

---
+++ old/routes/utils/DButils.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const MySql = require("./MySql");
+
+exports.execQuery = async function (query,params = []) {
+    let returnValue = []
+    const connection = await MySql.connection();
+    try {
+    await connection.query("START TRANSACTION");
+    returnValue = await connection.query(query,params);
+    await connection.query("COMMIT"); // WHY DIDNT WE HAD THIS BEFORE??????
+  } catch (err) {
+    await connection.query("ROLLBACK");
+    console.log('ROLLBACK at querySignUp', err);
+    throw err;
+  } finally {
+    await connection.release();
+  }
+  return returnValue
+}
+
